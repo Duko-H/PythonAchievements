86f6bea02d4f3e962f4b6d582fdbbf0701779610
Duko <dukovanherwijnen@gmail.com>
2020-09-03 07:20:32 -0800

    Een Poem gemaakt.

---
+++ PYTB1L2PoemPrinting/Poem.py
@@ -0,0 +1,8 @@
+Naam = "Mijn naam is Duko en dat rijmt een beetje op pluto,"
+Leren = "wie weet leer ik wel genoeg op MA om naar pluto te gaan."
+Banaan = "Ik hou niet heel veel van banaan,"
+Alkmaar = "maar die komen ook niet uit mijn stad vandaan."
+Klas = "Net zoals me klasgenoten," 
+Geslaagd = "hoop ik dat we op het eind hebben genoten."
+
+print(Naam, Leren, Banaan, Alkmaar, Klas, Geslaagd)
