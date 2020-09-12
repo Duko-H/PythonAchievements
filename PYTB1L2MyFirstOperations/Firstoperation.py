b6792e5970defd7b7fba3dc8f72e791320479f1e
Duko <dukovanherwijnen@gmail.com>
2020-09-11 14:51:05 -1000

    Reken verhaaltje

---
+++ PYTB1L2MyFirstOperations/Firstoperation.py
@@ -0,0 +1,51 @@
+naam = "Ik ben Duko"
+hallo = "Hallo,"
+rekenen = "Zal ik even rekenen"
+
+#
+import time
+#
+
+print(hallo, naam)
+
+time.sleep(1.0)
+
+print(rekenen)
+
+time.sleep(1.0)
+
+print("10 + 10 =")
+print(10 + 10)
+time.sleep(1.0)
+print("laten we wat moeilijker gaan rekenen")
+time.sleep(1.0)
+print("60 / 29 =")
+print(60 / 29)
+
+print("nog moeilijker")
+time.sleep(1.0)
+print("1609 - 1789 =")
+print(1609 - 1789)
+time.sleep(2.0)
+print("oh")
+time.sleep(1.0)
+print("dat kan niet")
+time.sleep(1.0)
+
+print("nog eentje dan")
+time.sleep(1.0)
+print("11 % 3 =")
+time.sleep(1.0)
+print(11 % 3)
+time.sleep(2.0)
+
+print("dit waren mijn reken kunsten, Doei!")
+time.sleep(3.0)
+
+quit()
+
+
+
+
+
+
