ef7de50d2407e31ab38d8bb04e2139ece881a7ae
Duko <dukovanherwijnen@gmail.com>
2020-09-11 14:20:14 -1000

    Removed helloworld.py

---
+++ PYTB1L2PlayerStatistics/Statistics.py
@@ -0,0 +1,16 @@
+#Knowledge, Strength enzv. hebben schaal van 0/100
+
+Player = Duko
+Knowledge = 75
+Strength = 40
+Speed = 65
+Magica = 80
+Special = Blackmagic
+Lives = 3
+Level = 1
+Charisma = 70
+Vehicle = Car
+
+
+
+
