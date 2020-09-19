e957f121c514d3e66eb5723d3a37d8c3da71e65e
Duko <dukovanherwijnen@gmail.com>
2020-09-19 02:45:30 +0200

    Stel_vragen af gemaakt

---
+++ PYTB1L2InterestingTellMeMore/stel_vragen.py
@@ -0,0 +1,8 @@
+naam = input("Hello what is your name? ")
+print("Aha so your name is", naam,"my name is Hendrik. nice to meet you!")
+
+age = input("How old are you if i may ask? ")
+print("So you are", age, "years old. I am 16 but age doesn't matter for friendships")
+
+hobby = input("What is your most fun hobby? ")
+print(hobby, "is your hobby? sounds fun i don't really have one.")
